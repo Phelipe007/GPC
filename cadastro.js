63bf69b6b3266040a5baa78f885c51cce7d28e22
henrique0Anjos <ha8558092@gmail.com>
2024-12-12 14:12:02 -0400

    verificação de preenchimento do formulário

---
+++ cadastro.js
@@ -0,0 +1,11 @@
+function modal() {
+    
+    Swal.fire({
+        timer: 2000,
+        icon: "error",
+        title: "Oops...",
+        showConfirmButton: false,
+        text: "Por favor, preencha todos os campos",
+    });
+    
+}
