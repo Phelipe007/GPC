46b04267cb5e9448015af0fac7dfd0c460a96753
henrique0Anjos <ha8558092@gmail.com>
2024-12-12 16:37:03 -0400

    perssonalizar a menssagem de erro do telefone

--- cadastro.js
+++ cadastro.js
@@ -1,11 +1,26 @@
 function modal() {
-    
+
     Swal.fire({
-        timer: 2000,
+        timer: 5000,
         icon: "error",
         title: "Oops...",
-        showConfirmButton: false,
+        showConfirmButton: true,
+        confirmButtonColor: "#006400",
         text: "Por favor, preencha todos os campos",
     });
-    
+
 }
+function verificacao() {
+
+    if (!/^\d{11}$/.test(document.getElementById("telefone").value)) {
+        Swal.fire({
+            timer: 5000,
+            icon: "Preencha o campo numero",
+            title: "Oops...",
+            showConfirmButton: true,
+            confirmButtonColor: "#006400",
+            text: "O número deve conter exatamente 11 dígitos.",
+        });
+
+    }
+};
