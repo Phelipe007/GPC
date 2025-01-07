e02ace5c1a25b2efa74060d1124f70630365b332
phelipe007 <phelipeifmt@gmail.com>
2025-01-07 18:18:53 -0400

    mudanças

---
+++ eventos/evento.php
@@ -0,0 +1,48 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eventos</title>
+</head>
+<body>
+    <header>
+        <h1>Paseio ciclistico</h1>
+    </header>
+    
+        <section>
+
+            <div class="container">
+
+                <h2>Data e Horário</h2>
+                <p>Data: </p>
+                <p>Horario: </p>
+
+                <h2>Local</h2>
+                <p>Data: </p>
+
+                <h2>Trajeto</h2>
+                <p>Percuso: </p>
+
+                <h2>Detalhes do Evento</h2>
+
+                <p></p>
+        
+                <input type="submit" value="Inscrever-se">
+                <p>Se inscreva para não perder esse dia incrível.</p>
+        
+
+            </div>
+    
+        </section>
+        <footer>
+            <p>&copy; 2025 Passeio Ciclístico.</p>
+        </footer>
+    
+
+</body>
+</html>
